d14059e4012b1374c83c3c16266cf77548621b1e
m-rakowski <michalrakowski@yahoo.com>
2017-04-24 17:44:28 +0200

    Add opening gsmarena in an embedded browser and a 'go back' button

--- app/src/main/java/course/android/android_lesson5/MainActivity.java
+++ app/src/main/java/course/android/android_lesson5/MainActivity.java
@@ -2,21 +2,22 @@ package course.android.android_lesson5;
 
 import android.content.Intent;
 import android.content.res.Resources;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
+
+
 
 public class MainActivity extends AppCompatActivity {
-    private String[] gsm_arena_urls;
-    private String[] phones;
+    private String[] gsm_arena_urls_from_strings_xml;
+    private String[] phones_from_strings_xml;
     private ListView lv;
 
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -26,26 +27,28 @@ public class MainActivity extends AppCompatActivity {
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v, int pos, long id) {
-                String url = gsm_arena_urls[pos];
-
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                startActivity(browserIntent);
+                openPhoneDisplayActivity(pos);
             }
         });
     }
 
+    private void openPhoneDisplayActivity(int pos) {
+
+        String url = gsm_arena_urls_from_strings_xml[pos];
+        Intent intent = new Intent(this, PhoneDisplayActivity.class);
+
+        intent.putExtra("urlToPhoneDescription", url);
+        startActivity(intent);
+    }
+
     private void initResources() {
         Resources res = getResources();
-        phones = res.getStringArray(R.array.phones);
-        gsm_arena_urls = res.getStringArray(R.array.gsmarena_urls);
+        phones_from_strings_xml = res.getStringArray(R.array.phones);
+        gsm_arena_urls_from_strings_xml = res.getStringArray(R.array.gsmarena_urls);
     }
 
     private void initLanguagesListView() {
         lv.setAdapter(new ArrayAdapter<>(getBaseContext(),
-                android.R.layout.simple_list_item_1, phones));
+                android.R.layout.simple_list_item_1, phones_from_strings_xml));
     }
-
-
 }
-
-
---
+++ app/src/main/java/course/android/android_lesson5/PhoneDisplayActivity.java
@@ -0,0 +1,55 @@
+package course.android.android_lesson5;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class PhoneDisplayActivity extends AppCompatActivity {
+
+    private Button goBackButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_display);
+
+        Intent intent = getIntent();
+        Bundle intentExtras = intent.getExtras();
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl((String) intentExtras.get("urlToPhoneDescription"));
+        myWebView.setWebViewClient(new Callback());  //HERE IS THE MAIN CHANGE
+
+        initButtons();
+    }
+    private class Callback extends WebViewClient {  //HERE IS THE MAIN CHANGE.
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return (false);
+        }
+
+    }
+    private void initButtons() {
+
+        goBackButton = (Button) findViewById(R.id.go_back_button);
+        goBackButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                goBack();
+            }
+        });
+
+    }
+
+    private void goBack() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
