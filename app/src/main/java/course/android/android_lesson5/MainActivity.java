e47dfc051118cfc376656a378e5052f465ade9b3
M R <M R>
2017-04-24 14:49:30 +0200

    Initial commit

---
+++ app/src/main/java/course/android/android_lesson5/MainActivity.java
@@ -0,0 +1,13 @@
+package course.android.android_lesson5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
